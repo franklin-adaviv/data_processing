97ee55d5f7063db958e56a9a2c5b3683c7e13ba8
franklin <franklin@adaviv.com>
2020-07-30 14:40:55 -0700

    extracted data

--- src/rosbag_reader.py
+++ src/rosbag_reader.py
@@ -23,11 +23,36 @@ def process_pose_data(file):
 			(topic, msg, t) = message_data
 
 			# topic names
-			a_topic = "something"
-			b_topic = "something"
-			
+			a_topic = "/device_0/sensor_0/Pose_0/pose/transform/data"
+			b_topic = "/device_0/sensor_0/Pose_0/pose/transform/data"
+			c_topic = "/device_0/sensor_0/Pose_0/pose/metadata"
+				
+			# time
+			#print(t)
+			timestamp = t
+
+			# cases
 			if topic == a_topic:
 				# get pose 
+				#print(msg)
+				t_x = float(msg.translation.x)
+				t_y = float(msg.translation.y)
+				t_z = float(msg.translation.z)
+
+				r_x = float(msg.rotation.x)
+				r_y = float(msg.rotation.y)
+				r_z = float(msg.rotation.z)
+				r_w = float(msg.rotation.w)
+
+				print(t_x,t_y,t_z,r_x,r_y,r_z,r_w)
+				print(np.linalg.norm(np.array([r_x,r_y,r_z,r_w])))
+
+			elif topic == b_topic:
+				#print(msg)
+				pass
+			elif topic == c_topic:
+				#print(msg)
+				pass
 
 
 		except:
@@ -36,4 +61,6 @@ def process_pose_data(file):
 
 
 if __name__ == "__main__":
+	b1 = "data/20200728_164058.bag"
+	process_pose_data(b1)
 
